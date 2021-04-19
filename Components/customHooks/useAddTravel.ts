ffd72acae0a9367ba75d53abd6d3efbf32122ca7
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2021-04-19 12:45:53 +0200

    Remove unused code

--- Components/customHooks/useAddTravel.ts
+++ Components/customHooks/useAddTravel.ts
@@ -1,7 +1,5 @@
-import { useMutation } from '@apollo/client';
 import { useReducer } from 'react';
 
-import { ADD_TRAVEL } from '../../lib/graphql/client/mutations';
 import { ImagesActionsTypes } from '../../useReducers/imagesReducer/imagesActionsTypes';
 import { reducer, initialState } from '../../useReducers/imagesReducer/imagesReducer';
 
