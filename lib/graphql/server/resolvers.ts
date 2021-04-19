e03f61bd86f0ca4400e8328c0b6e40c0350421a6
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2021-04-19 12:47:17 +0200

    Move code involnving logging user in

--- lib/graphql/server/resolvers.ts
+++ lib/graphql/server/resolvers.ts
@@ -1,8 +1,11 @@
 import bcrypt from 'bcrypt';
-import {query} from '../../db/db';
 import jwt from 'jsonwebtoken';
-import arg from "arg";
+import validator from 'validator'
+import { UserInputError, AuthenticationError } from 'apollo-server-micro'
 
+import {query} from '../../db/db';
+import { UserType } from '../../types/types';
+import { GlobalErrors, LoginErrors } from './errors';
 
 const resolvers = {
     Mutation: {
@@ -29,6 +32,33 @@ const resolvers = {
                 token
             };
 
+        },
+        loginUser: async (_ : unknown, args: { email : string, password: string }) => {
+            try {
+                const regex = /(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/
+                if (!validator.isEmail(args.email) || !regex.test(args.password))
+                    throw new UserInputError(LoginErrors.WRONG_CREDENTIALS)
+                
+                const user = await query('SELECT id, password FROM users WHERE email = ? AND verified = 1', [args.email])
+                if (user.length === 0) throw new AuthenticationError(LoginErrors.WRONG_CREDENTIALS)
+
+                const match = await bcrypt.compare(args.password, (user[0] as UserType).password)
+                if (!match) throw new AuthenticationError(LoginErrors.WRONG_CREDENTIALS)
+
+                const refreshToken = jwt.sign({ id: (user[0] as UserType).id }, `${process.env.REFRESH_TOKEN_SECRET}`)
+                const accessToken = jwt.sign({ id: (user[0] as UserType).id }, `${process.env.ACCESS_TOKEN_SECRET}`, { expiresIn: '5m' })
+
+                const insertRefreshToken = await query('INSERT INTO refresh_tokens VALUES (NULL, ?, ?)', [refreshToken, (user[0] as UserType).id])
+                if (insertRefreshToken.affectedRows !== 1) throw new Error()
+
+                return {
+                    refreshToken,
+                    accessToken
+                }
+            }
+            catch (e) {
+                throw new Error(e.message ? e.message : GlobalErrors.STH_WENT_WRONG)
+            }
         }
     }
 }
